25fa9585f6dc8356e72a6b57b7b06e70321845c3
Ashlyn <ashlyn@Admins-MacBook-Pro.local>
2019-06-05 15:56:01 -0600

    added meme component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import {Meme} from './components/meme';
 
 function App() {
   const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
 
   useEffect(() => {
     fetch('https://api.imgflip.com/get_memes')
-      .then(res => {
-        res.json()
-          .then(data => {
-            setTemplates(data.data.memes)
-            console.log(templates)
-          })
-      })
+      .then(x => x.json().then(res => setTemplates(res.data.memes)))
   }, [])
 
-  return <div style={{ textAlign: "center" }}>hi</div>
+  return <div style={{ textAlign: "center" }}>
+    {template && <Meme template={template}/>}
+    {!template && templates.map(template => {
+      return (
+        <Meme template={template} onClick={() => setTemplate(template)}/>
+      )
+    })}
+  </div>
 }
 
 export default App;
