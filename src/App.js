013c6ece63dfce676cf8750caa91e4b22a66da3d
Ashlyn <ashlyn@Admins-MacBook-Pro.local>
2019-06-05 13:43:56 -0600

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [templates, setTemplates] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(res => {
+        res.json()
+          .then(data => {
+            setTemplates(data.data.memes)
+            console.log(templates)
+          })
+      })
+  }, [])
+
+  return <div style={{ textAlign: "center" }}>hi</div>
 }
 
 export default App;
